fd0e36c8d8e455d9857f90f69d8f2b22cd7e27e5
saifmohammad00 <saifmohammad592001@gmail.com>
2024-07-05 22:12:53 +0530

    add summary to the restaurant website

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,11 +6,22 @@ const Header = () => {
     return <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton/>
+            <HeaderCartButton />
         </header>
         <div className={classes['top-image']}>
             <img src={mealsimg} alt="page header" />
         </div>
+        <div className={classes.summary}>
+                <h2>Delicious Food, Delivered To You</h2>
+                <p>
+                    Choose your favorite meal from our broad selection of available meals
+                    and enjoy a delicious lunch or dinner at home.
+                </p>
+                <p>
+                    All our meals are cooked with high-quality ingredients, just-in-time and
+                    of course by experienced chefs!
+                </p>
+            </div>
     </Fragment>
 }
 export default Header;
