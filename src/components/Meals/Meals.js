52f5bdd1beb01edc9a5c2524c9ee23031c89a2cc
saifmohammad00 <saifmohammad592001@gmail.com>
2024-07-06 22:34:45 +0530

    Add form

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,41 +1,54 @@
 
 import classes from "./Meals.module.css";
-const Meals=()=>{
-    const MealsList=[
+const Meals = () => {
+    const MealsList = [
         {
             id: 'm1',
             name: 'Sushi',
             description: 'Finest fish and veggies',
             price: "$22.99",
-          },
-          {
+        },
+        {
             id: 'm2',
             name: 'Schnitzel',
             description: 'A german specialty!',
             price: "$16.50",
-          },
-          {
+        },
+        {
             id: 'm3',
             name: 'Barbecue Burger',
             description: 'American, raw, meaty',
             price: "$12.99"
-          },
-          {
+        },
+        {
             id: 'm4',
             name: 'Green Bowl',
             description: 'Healthy...and green...',
             price: "$18.99",
-          },
+        },
     ];
-    const List=MealsList.map((meal)=>
-                <section>
-                    <strong>{meal.name}</strong>
-                    <div>{meal.description}</div>
-                    <strong style={{color:"#8a2b06"}}>{meal.price}</strong>
-                    <hr></hr>
-                </section>)
+    const List = MealsList.map((meal) =>
+        <>
+        <section className={classes.box}>
+            <div className={classes.section}>
+                <strong>{meal.name}</strong>
+                <div>{meal.description}</div>
+                <strong style={{ color: "#8a2b06" }}>{meal.price}</strong>
+            </div>
+            <div className={classes.amount}>
+                <form>
+                    <div>
+                    <strong>Amount</strong>
+                    <input type="number" defaultValue={1} />
+                    </div>
+                    <button>+ Add</button>
+                </form>
+            </div>
+        </section>
+        <hr></hr>
+        </>)
     return <div className={classes.mealsection}>
-            {List}
+        {List}
     </div>
-    }
+}
 export default Meals;
